39f8f4d992714c8836e578f4e5b52d0b8f033124
Nuzhat Bushra <nuzhatbushra@Nuzhats-Air.lan>
2024-05-30 13:07:22 -0400

    going over next.js basics, added pages,react component, and css page

---
+++ src/app/components/Header.js
@@ -0,0 +1,14 @@
+//practice example
+import Styles from "@app/app/styles/Page.module.css"
+export default function Header({name}){
+  return(
+    <>
+      <h1 className={Styles.h1}>
+        Example heading by {name}
+      </h1>
+      <p className={Styles.p}>
+        The world is round, or is it??
+      </p>
+    </>
+  )
+}
