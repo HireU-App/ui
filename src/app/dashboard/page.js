350ea973b0d6f1ccffb3596f17e12ea9eed90289
Pablo Najera <pablo@najera.cc>
2024-06-01 11:36:33 -0400

    adding lint setup, lint staged

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,15 +1,13 @@
 //Practice example
-import Styles from "@app/app/styles/Page.module.css"
-import Header from "@app/app/components/Header"
+import Styles from "@app/app/styles/Page.module.css";
+import Header from "@app/app/components/Header";
 
-export default function Dashboard (){
-return (
-  <>
-  <Header name= "Joseann" className={Styles.border}></Header>
-  <Header name = "Nuzhat"></Header>
-  <p>
-    hellp from Dashboard
-  </p>
-  </>
-)
+export default function Dashboard() {
+  return (
+    <>
+      <Header name="Joseann" className={Styles.border}></Header>
+      <Header name="Nuzhat"></Header>
+      <p>hellp from Dashboard</p>
+    </>
+  );
 }
