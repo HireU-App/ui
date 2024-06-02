066abce2a8f9bd5006b88230783a16b1af4a8deb
Nuzhat Bushra <nuzhatbushra@Nuzhats-Air.lan>
2024-06-02 13:48:56 -0400

    installed proptypes but it is not giving error

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,14 +1,22 @@
 //practice example
 import Styles from "@app/app/styles/Page.module.css"
-export default function Header({name}){
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Header({name, year}){
   return(
     <>
       <h1 className={Styles.h1}>
-        Example heading by {name}
+        Example heading by {name} in year {year}
       </h1>
       <p className={Styles.p}>
         The world is round, or is it??
       </p>
     </>
   )
+}
+
+Header.propTypes={
+  name: PropTypes.number.isRequired,
+  year: PropTypes.number
 }
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,12 +1,13 @@
 //Practice example
 import Styles from "@app/app/styles/Page.module.css"
 import Header from "@app/app/components/Header"
-
+import React from 'react';
+import PropTypes from 'prop-types';
 export default function Dashboard (){
 return (
   <>
-  <Header name= "Joseann" className={Styles.border}></Header>
-  <Header name = "Nuzhat"></Header>
+  <Header name= {[12,13]} className={Styles.border}></Header>
+  <Header name = "Nuzhat" year= "2021"></Header>
   <p>
     hellp from Dashboard
   </p>
