280fd3717f9a4f16a5cb6726169760a4c7ef079c
J-O-S-I-E <128793939+J-O-S-I-E@users.noreply.github.com>
2024-06-02 13:43:06 -0400

    Installed Ant design components and also added a button to page.js and setup layout for web pages

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,6 +1,8 @@
 //Practice example
 import Styles from "@app/app/styles/Page.module.css";
 import Header from "@app/app/components/Header";
+import React from "react";
+import { Button } from "antd";
 
 export default function Dashboard() {
   return (
@@ -8,6 +10,7 @@ export default function Dashboard() {
       <Header name="Joseann" className={Styles.border}></Header>
       <Header name="Nuzhat"></Header>
       <p>hellp from Dashboard</p>
+      <Button type="primary">Button</Button>
     </>
   );
 }
