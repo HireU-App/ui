ce298fe3d7442b50be185318042a3cc16879c67b
Pablo Najera <pablo@najera.cc>
2024-06-05 18:49:51 -0400

    adding zod-props for prop validation

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,14 +1,14 @@
 //Practice example
-import Styles from "@app/app/styles/Page.module.css";
-import Header from "@app/app/components/Header";
-import React from "react";
-import { Button } from "antd";
+import Styles from '@app/styles/Page.module.css';
+import Header from '@app/components/Header';
+import React from 'react';
+import { Button } from 'antd';
 
 export default function Dashboard() {
   return (
     <>
       <Header name="Joseann" className={Styles.border}></Header>
-      <Header name="Nuzhat"></Header>
+      <Header name="Nuzhat" number={490}></Header>
       <p>hellp from Dashboard</p>
       <Button type="primary">Button</Button>
     </>
