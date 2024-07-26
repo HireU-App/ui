6148a8edef965ef5e853fdc2a7c1a4426ea34d51
Nuzhat Bushra <ntb5562@nyu.edu>
2024-07-26 09:35:51 -0400

    changed landing page

--- app/page.js
+++ app/page.js
@@ -5,14 +5,15 @@ import { Image } from 'antd';
 export default function Home() {
   return (
     <div className={Styles.body}>
-      <div className={Styles.container}>
-        <h1>HireU: Student Skills at Your Service</h1>
+      <div className={Styles.mg20}>
+        <h1 className={Styles.mg20}>HireU: Student Skills at Your Service</h1>
         <Image
+          className={Styles.mg20}
           width={200}
           src="https://plus.unsplash.com/premium_vector-1682298611076-a3a4ffe57584?bg=FFFFFF&q=80&w=2782&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Image about university life from unsplash"
         ></Image>
-        <Text>
+        <Text className={Styles.mg20}>
           Hire U is a Web App that connects university students who have
           freelance skills with people looking for help. Do you need assistance
           moving, a tutor, or someone to simply manage your clubs social media?
